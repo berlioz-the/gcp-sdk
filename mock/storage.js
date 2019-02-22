b05cf82ad650d6bdf9f4b854a5b6c0463e47faec
Ruben Hakopian <ruben.hakopian@gmail.com>
2019-02-22 15:38:45 -0800

    updates to storage mocks

--- mock/storage.js
+++ mock/storage.js
@@ -12,13 +12,14 @@ var GcpSdkClient = require("../index");
 var client = new GcpSdkClient(logger, 'us-central1-a', credentials);
 
 var newBucketMeta = {
-    location: 'us-central1',
-    regional: true
+    location: 'US-CENTRAL1',
+    // regional: true
+    storageClass: 'REGIONAL'
 }
 
-return client.Storage.queryAllBuckets('dsample-proj-2-230121-gprod-')
+// return client.Storage.queryAllBuckets('dsample-proj-2-230121-gprod-zzzzz')
 // return client.Storage.queryBucket('dsample-proj-2-230121-gprod-aaa-bbb-ccc-ddd-eee-11')
-// return client.Storage.createBucket('dsample-proj-2-230121-gprod-aaa-bbb-ccc-ddd-eee-11', newBucketMeta)
+return client.Storage.createBucket('dsample-proj-2-230121-gprod-aaa-bbb-ccc-ddd-eee-11', newBucketMeta)
 // return client.Storage.deleteBucket('dsample-proj-2-230121-gprod-aaa-bbb-ccc-ddd-eee-11')
     .then(result => {
         // result = result.map(x => x.name);
