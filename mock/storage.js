46d038e26e782433f3cbf470f98d1f0976f9b7ff
Ruben Hakopian <ruben.hakopian@gmail.com>
2019-02-26 20:17:12 -0800

    minor fixes.

--- mock/storage.js
+++ mock/storage.js
@@ -12,15 +12,17 @@ var GcpSdkClient = require("../index");
 var client = new GcpSdkClient(logger, 'us-central1-a', credentials);
 
 var newBucketMeta = {
-    location: 'US-CENTRAL1',
-    // regional: true
-    storageClass: 'REGIONAL'
-}
+    "location": "US-CENTRAL1",
+    "storageClass": "REGIONAL",
+    "labels": {
+      "berlioz_cluster": "hello"
+    }
+  }
 
-// return client.Storage.queryAllBuckets('dsample-proj-2-230121-gprod-zzzzz')
+// return client.Storage.queryAllBuckets()
 // return client.Storage.queryBucket('dsample-proj-2-230121-gprod-aaa-bbb-ccc-ddd-eee-11')
-return client.Storage.createBucket('dsample-proj-2-230121-gprod-aaa-bbb-ccc-ddd-eee-11', newBucketMeta)
-// return client.Storage.deleteBucket('dsample-proj-2-230121-gprod-aaa-bbb-ccc-ddd-eee-11')
+// return client.Storage.createBucket('sample-proj-2-230121_localhomepc_hello_us-central1_main_users', newBucketMeta)
+return client.Storage.deleteBucket('sample-proj-2-230121_localhomepc_hello_us-central1_main_users')
     .then(result => {
         // result = result.map(x => x.name);
         // result = result.map(x => x.metadata);
