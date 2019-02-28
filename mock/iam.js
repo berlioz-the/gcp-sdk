c4d513b39355bef266bbfd8beae1e60ebf236347
Ruben Hakopian <ruben.hakopian@gmail.com>
2019-02-28 15:03:24 -0800

    updates to IAM apis

--- mock/iam.js
+++ mock/iam.js
@@ -11,11 +11,13 @@ var GcpSdkClient = require("../index");
 
 var client = new GcpSdkClient(logger, 'us-central1-a', credentials);
 
-//return client.Iam.queryAllServiceAccounts()
-// return client.Iam.createServiceAccount('another-service-acc')
+return client.Iam.queryAllServiceAccounts('gprod-gprod-hello-')
+// return client.Iam.queryLiveServiceAccounts('gprod-gprod-hello-')
+// return client.Iam.queryServiceAccount('projects/sample-proj-2-230121/serviceAccounts/gprod-gprod-hello-main-web-1@sample-proj-2-230121.iam.gserviceaccount.com')
+// return client.Iam.createServiceAccount('gprod-gprod-hello-main-web')
 // return client.Iam.deleteServiceAccount('projects/sample-proj-2-230121/serviceAccounts/another-service-acc@sample-proj-2-230121.iam.gserviceaccount.com')
-return client.Iam.queryAllServiceAccountKeys('projects/sample-proj-2-230121/serviceAccounts/another-service-acc@sample-proj-2-230121.iam.gserviceaccount.com')
-// return client.Iam.createServiceAccountKey('projects/sample-proj-2-230121/serviceAccounts/another-service-acc@sample-proj-2-230121.iam.gserviceaccount.com')
+// return client.Iam.queryAllServiceAccountKeys('projects/sample-proj-2-230121/serviceAccounts/gprod-gprod-hello-main-web-290@sample-proj-2-230121.iam.gserviceaccount.com')
+// return client.Iam.createServiceAccountKey('projects/sample-proj-2-230121/serviceAccounts/gprod-gprod-hello-main-web@sample-proj-2-230121.iam.gserviceaccount.com')
 //return client.Iam.deleteServiceAccountKey('projects/sample-proj-2-230121/serviceAccounts/another-service-acc@sample-proj-2-230121.iam.gserviceaccount.com/keys/4d563e87af99d7c6a839e31d395c49828515c882')
     .then(result => {
         // result = result.map(x => x.name);
@@ -23,5 +25,6 @@ return client.Iam.queryAllServiceAccountKeys('projects/sample-proj-2-230121/serv
         logger.info('RESULT: ', result);
     })
     .catch(reason => {
+        logger.error(reason.code);
         logger.error(reason);
     })
