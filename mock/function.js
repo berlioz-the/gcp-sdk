7937fc56892e23de7b7f62b36cea149bacee307f
Ruben Hakopian <ruben.hakopian@gmail.com>
2019-03-13 00:07:01 -0700

    added function wait for operation.

--- mock/function.js
+++ mock/function.js
@@ -12,24 +12,24 @@ var GcpSdkClient = require("../index");
 var client = new GcpSdkClient(logger, 'us-central1-a', credentials);
 
 // return client.Function.queryAllFunctions('sample-proj-2-230121', 'gprod-')
-return client.Function.queryFunction('projects/sample-proj-2-230121/locations/us-central1/functions/gprod-func-us-central1-main-hello')
+// return client.Function.queryFunction('projects/sample-proj-2-230121/locations/us-central1/functions/gprod-func-us-central1-main-hello')
 // return client.Function.deleteFunction('projects/sample-proj-2-230121/locations/us-central1/functions/function-hello')
-// return client.Function.createFunction('sample-proj-2-230121', 'hello-zzz', {
-//     "httpsTrigger": {
-//       "url": "https://us-central1-sample-proj-2-230121.cloudfunctions.net/hello-zzz"
-//     },
-//     "status": "ACTIVE",
-//     "entryPoint": "handler",
-//     "timeout": "60s",
-//     "availableMemoryMb": 256,
-//     // "serviceAccountEmail": "sample-proj-2-230121@appspot.gserviceaccount.com",
-//     "versionId": "1",
-//     "labels": {
-//         'berlioz-cluster': 'hello'
-//     },
-//     "sourceArchiveUrl": "gs://sample-proj-2-230121-imagestore-func/func/lambda-func-main-hello/sha256:c432de6022e725e767e6ac80263d79f0f428df23a62afa100162081e4b0df8f8",
-//     "runtime": "nodejs6"
-// })
+return client.Function.createFunction('sample-proj-2-230121', 'hello-aaa', {
+    "httpsTrigger": {
+      "url": "https://us-central1-sample-proj-2-230121.cloudfunctions.net/hello-aaa"
+    },
+    "status": "ACTIVE",
+    "entryPoint": "handler",
+    "timeout": "60s",
+    "availableMemoryMb": 256,
+    // "serviceAccountEmail": "sample-proj-2-230121@appspot.gserviceaccount.com",
+    "versionId": "1",
+    "labels": {
+        'berlioz-cluster': 'hello'
+    },
+    "sourceArchiveUrl": "gs://sample-proj-2-230121-imagestore-func/func/lambda-func-main-hello/sha256:c432de6022e725e767e6ac80263d79f0f428df23a62afa100162081e4b0df8f8",
+    "runtime": "nodejs6"
+})
 // return client.Function.updateFunction('projects/sample-proj-2-230121/locations/us-central1/functions/gprod-func-us-central1-main-hello', {
 //     "httpsTrigger": {
 //       "url": "https://us-central1-sample-proj-2-230121.cloudfunctions.net/gprod-func-us-central1-main-hello"
@@ -46,7 +46,29 @@ return client.Function.queryFunction('projects/sample-proj-2-230121/locations/us
 //     "sourceArchiveUrl": "gs://sample-proj-2-230121-imagestore-func/func/lambda-func-main-hello/sha256:c432de6022e725e767e6ac80263d79f0f428df23a62afa100162081e4b0df8f8",
 //     "runtime": "nodejs6"
 // })
+.then(result => client.Function.waitForOperation(result))
+// .then(result => {
+//     logger.info('INITIAL UPDATE RESULT: ', result);
+
+//     return  client.Function.updateFunction('projects/sample-proj-2-230121/locations/us-central1/functions/gprod-func-us-central1-main-hello', {
+//         "httpsTrigger": {
+//           "url": "https://us-central1-sample-proj-2-230121.cloudfunctions.net/gprod-func-us-central1-main-hello"
+//         },
+//         "status": "ACTIVE",
+//         "entryPoint": "handler",
+//         "timeout": "60s",
+//         "availableMemoryMb": 256,
+//         // "serviceAccountEmail": "sample-proj-2-230121@appspot.gserviceaccount.com",
+//         "versionId": "1",
+//         "labels": {
+//             'berlioz-cluster': 'hello'
+//         },
+//         "sourceArchiveUrl": "gs://sample-proj-2-230121-imagestore-func/func/lambda-func-main-hello/sha256:c432de6022e725e767e6ac80263d79f0f428df23a62afa100162081e4b0df8f8",
+//         "runtime": "nodejs6"
+//     })
+// })
     // return client.Function.stabilizeFunction('projects/sample-proj-2-230121/locations/us-central1/functions/gprod-func-us-central1-main-hello')
+    // return client.Function.getOperationStatus('operations/c2FtcGxlLXByb2otMi0yMzAxMjEvdXMtY2VudHJhbDEvZ3Byb2QtZnVuYy11cy1jZW50cmFsMS1tYWluLWhlbGxvL3dRYndJc2ZxclZj')
     .then(result => {
         // result = result.map(x => x.name);
         // result = result.map(x => x.metadata);
