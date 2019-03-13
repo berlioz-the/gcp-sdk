10e96519f0a8a43e1f0e4803ee21397412cc803d
Ruben Hakopian <ruben.hakopian@gmail.com>
2019-03-13 01:11:26 -0700

    added operation to delete function

--- mock/function.js
+++ mock/function.js
@@ -13,23 +13,23 @@ var client = new GcpSdkClient(logger, 'us-central1-a', credentials);
 
 // return client.Function.queryAllFunctions('sample-proj-2-230121', 'gprod-')
 // return client.Function.queryFunction('projects/sample-proj-2-230121/locations/us-central1/functions/gprod-func-us-central1-main-hello')
-// return client.Function.deleteFunction('projects/sample-proj-2-230121/locations/us-central1/functions/function-hello')
-return client.Function.createFunction('sample-proj-2-230121', 'hello-aaa', {
-    "httpsTrigger": {
-      "url": "https://us-central1-sample-proj-2-230121.cloudfunctions.net/hello-aaa"
-    },
-    "status": "ACTIVE",
-    "entryPoint": "handler",
-    "timeout": "60s",
-    "availableMemoryMb": 256,
-    // "serviceAccountEmail": "sample-proj-2-230121@appspot.gserviceaccount.com",
-    "versionId": "1",
-    "labels": {
-        'berlioz-cluster': 'hello'
-    },
-    "sourceArchiveUrl": "gs://sample-proj-2-230121-imagestore-func/func/lambda-func-main-hello/sha256:c432de6022e725e767e6ac80263d79f0f428df23a62afa100162081e4b0df8f8",
-    "runtime": "nodejs6"
-})
+return client.Function.deleteFunction('projects/sample-proj-2-230121/locations/us-central1/functions/hello-aaa')
+// return client.Function.createFunction('sample-proj-2-230121', 'hello-aaa', {
+//     "httpsTrigger": {
+//       "url": "https://us-central1-sample-proj-2-230121.cloudfunctions.net/hello-aaa"
+//     },
+//     "status": "ACTIVE",
+//     "entryPoint": "handler",
+//     "timeout": "60s",
+//     "availableMemoryMb": 256,
+//     // "serviceAccountEmail": "sample-proj-2-230121@appspot.gserviceaccount.com",
+//     "versionId": "1",
+//     "labels": {
+//         'berlioz-cluster': 'hello'
+//     },
+//     "sourceArchiveUrl": "gs://sample-proj-2-230121-imagestore-func/func/lambda-func-main-hello/sha256:c432de6022e725e767e6ac80263d79f0f428df23a62afa100162081e4b0df8f8",
+//     "runtime": "nodejs6"
+// })
 // return client.Function.updateFunction('projects/sample-proj-2-230121/locations/us-central1/functions/gprod-func-us-central1-main-hello', {
 //     "httpsTrigger": {
 //       "url": "https://us-central1-sample-proj-2-230121.cloudfunctions.net/gprod-func-us-central1-main-hello"
