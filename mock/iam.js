7278d9542bd00f45a6780ed46befbed1226abc8d
Ruben Hakopian <ruben.hakopian@gmail.com>
2019-03-18 12:08:00 -0700

    added project iam policy

--- mock/iam.js
+++ mock/iam.js
@@ -11,7 +11,9 @@ var GcpSdkClient = require("../index");
 
 var client = new GcpSdkClient(logger, 'us-central1-a', credentials);
 
-return client.Iam.queryAllServiceAccounts('gprod-berlioz-')//)
+return client.Iam.getProjectIamPolicy()
+// return client.Iam.queryProjectPolicy()
+// return client.Iam.queryAllServiceAccounts('gprod-berlioz-')//)
 // return client.Iam.queryLiveServiceAccounts('gprod-gprod-hello-')
 // return client.Iam.queryServiceAccount('projects/sample-proj-2-230121/serviceAccounts/gprod-berlioz-main-ctlr-1@sample-proj-2-230121.iam.gserviceaccount.com')
 // return client.Iam.createServiceAccount('gprod-gprod-hello-main-web')
