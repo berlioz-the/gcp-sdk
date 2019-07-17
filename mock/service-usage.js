5375a88a4d6285cabae0cd312240a071c0491f7b
Ruben Hakopian <ruben.hakopian@gmail.com>
2019-07-16 21:03:43 -0700

    mock

--- mock/service-usage.js
+++ mock/service-usage.js
@@ -6,7 +6,8 @@ var logger = require('the-logger').setup('gcp-sdk',
 logger.level = 'silly';
 
 var Promise = require('the-promise');
-var credentials = require('./credentials.json');
+// var credentials = require('./credentials.json');
+var credentials = require('./johnbalvin-credentials.json');
 
 var GcpSdkClient = require("../index");
 
@@ -27,17 +28,17 @@ function waitOperation(name)
 
 // return client.ServiceUsage.queryAll()
 // return client.CloudResourceManager.queryProject()
-// return client.ServiceUsage.query('zync.googleapis.com')
+return client.ServiceUsage.query('cloudresourcemanager.googleapis.com')
 // return client.ServiceUsage.enable('zync.googleapis.com')
-// return client.ServiceUsage.disable('zync.googleapis.com')
+// return client.ServiceUsage.enable('cloudresourcemanager.googleapis.com')
 // return client.ServiceUsage.disable('container.googleapis.com')
 // return client.ServiceUsage.disable('containerregistry.googleapis.com')
-return client.ServiceUsage.disable('cloudresourcemanager.googleapis.com')
+// return client.ServiceUsage.disable('cloudresourcemanager.googleapis.com')
 // return client.ServiceUsage.enable('cloudresourcemanager.googleapis.com')
 .then(result => {
     console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
     console.log(result);
-    return waitOperation(result.name);
+    // return waitOperation(result.name);
 })
 
 // return client.ServiceUsage.getOperation('operations/acf.5c816650-ca81-4a32-a2a2-a90dfb46172e')
